ff70698618fd99b973a7824acedc470d7a2c4861
Karam-a <81566216+Karam-a@users.noreply.github.com>
2024-10-22 15:17:19 +0200

    First Commit (Yani första)

--- Poddprojekt Grupp24/PoddprojektGrupp24/Properties/Resources.Designer.cs
+++ Poddprojekt Grupp24/PoddprojektGrupp24/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PoddprojektGrupp24.Properties {
+namespace PresentationLayer.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PoddprojektGrupp24.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PoddprojektGrupp24.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PresentationLayer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
