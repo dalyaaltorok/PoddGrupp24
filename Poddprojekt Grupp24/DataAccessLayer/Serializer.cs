5c595b85aac3b6ddfd308c9809b0780b56884ae2
Dalya <121013171+dalyaaltorok@users.noreply.github.com>
2024-10-23 12:15:53 +0200

    test

--- Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
+++ Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
@@ -6,23 +6,38 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using System.Windows.Forms;
 
 namespace DataAccessLayer
 {
     public class Serializer<T>
     {
         private string path { get; set; }
+
+        ValidationSerializer validation;
         
         public Serializer (string aPath)
         {
             path = aPath;
+            validation = new ValidationSerializer();
         }
         public void Serialize (List<T> list)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
-            using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            try
             {
-                serializer.Serialize(stream, list);
+                if (!validation.DirectoryExists(path))
+                {
+                    MessageBox.Show("Directory does not exist.", "Error");
+                }
+                XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+                using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    serializer.Serialize(stream, list);
+                }
+            }
+            catch
+            {
+
             }
         }
         public List<T> Deserialize()
---
+++ Poddprojekt Grupp24/DataAccessLayer/ValidationSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ValidationSerializer
+    {
+        public bool DirectoryExists(string path)
+        {
+            return Directory.Exists(Path.GetDirectoryName(path));
+        }
+
+        public bool FileExists(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+}
