b8e1fc9cff07b76d5835ed131155e2b39f2a68a7
Karam-a <81566216+Karam-a@users.noreply.github.com>
2024-10-23 12:26:50 +0200

    Lade till Serializer Klassen

--- Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
+++ Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
@@ -3,11 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Windows.Forms;
 
 namespace DataAccessLayer
 {
-    public class Serializer
+    public class Serializer<T>
     {
+        private string path { get; set; }
 
+        ValidationSerializer validation;
+
+        public Serializer(string aPath)
+        {
+            path = aPath;
+            validation = new ValidationSerializer();
+        }
+        public void Serialize(List<T> list)
+        {
+            try
+            {
+                if (!validation.DirectoryExists(path))
+                {
+
+                }
+                XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+                using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    serializer.Serialize(stream, list);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        public List<T> Deserialize()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<T>));
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                List<T> list = (List<T>)deserializer.Deserialize(stream);
+                return list;
+            }
+        }
     }
 }
