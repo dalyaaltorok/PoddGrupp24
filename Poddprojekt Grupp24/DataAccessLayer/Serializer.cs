4bfa8e6117357dcafe6e49d53e029384b77362f3
Dalya <121013171+dalyaaltorok@users.noreply.github.com>
2024-10-23 10:58:14 +0200

    Började på serialiseringsklassen

--- Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
+++ Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace DataAccessLayer
 {
-    public class Serializer
+    public class Serializer<T>
     {
-
+        private string path { get; set; }
+        
+        public Serializer (string aPath)
+        {
+            path = aPath;
+        }
+        public void Serialize (List<T> list)
+        {
+            
+        }
     }
 }
