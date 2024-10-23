2522447479a3b4dee1e535b58a443348b28e807d
Karam-a <81566216+Karam-a@users.noreply.github.com>
2024-10-23 14:53:07 +0200

    Skapat Models som class library Lagt till "category" och "Episode"-klasser

---
+++ Poddprojekt Grupp24/Models/Episode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Episode
+    {
+        private string name;
+        public string Name 
+        { 
+            get { return name; } 
+            set { name = value; }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public Episode(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+    }
+}
