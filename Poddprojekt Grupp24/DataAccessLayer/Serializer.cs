106baf81ae377635d1755f3a51f4e9695b48616c
Karam-a <81566216+Karam-a@users.noreply.github.com>
2024-10-23 10:27:27 +0200

    La till "using" direktivet i de klasser som saknade det.

--- Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
+++ Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
@@ -1,4 +1,10 @@
-﻿namespace DataAccessLayer
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
 {
     public class Serializer
     {
