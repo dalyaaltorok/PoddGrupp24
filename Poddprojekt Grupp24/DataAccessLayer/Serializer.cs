8c889000c43cc0cc89480c70dbbe6fac56997e68
Dalya <121013171+dalyaaltorok@users.noreply.github.com>
2024-10-23 11:10:33 +0200

    Skrev klart serializer, ska lägga till felhantering

--- Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
+++ Poddprojekt Grupp24/DataAccessLayer/Serializer.cs
@@ -19,7 +19,20 @@ namespace DataAccessLayer
         }
         public void Serialize (List<T> list)
         {
-            
+            XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+            using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                serializer.Serialize(stream, list);
+            }
+        }
+        public List<T> Deserialize()
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<T>));
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                List<T> list = (List<T>)deserializer.Deserialize(stream);
+                return list;
+            }
         }
     }
 }
