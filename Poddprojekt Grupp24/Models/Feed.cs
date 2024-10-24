080b1ef34fe408c4198cd9b4c41d5639b393adcd
Karam-a <81566216+Karam-a@users.noreply.github.com>
2024-10-24 13:19:48 +0200

    Lade till innehåll i Feed-klassen

--- Poddprojekt Grupp24/Models/Feed.cs
+++ Poddprojekt Grupp24/Models/Feed.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class Feed
+    public class Feed
     {
+        private string name;
+        private string title;
+        private Category category;
+        private List<Episode> episodes;
+        public string Name;
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public Category category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        public List<Episode> episodes
+        {
+            get { return episodes; }
+            set { episodes = value; }
+        }
+
+        public Feed(string name, string title, Category category, List<Episode> episodes)
+        {
+            this.name = name;
+            this.title = title;
+            this.category = category;
+            this.episodes = episodes;
+        }
+
+        public Feed()
+        {
+
+        }
+
     }
 }
